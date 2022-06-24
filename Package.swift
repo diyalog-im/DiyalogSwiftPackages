ca280c7a02aeed4505d22490672c5e873a75b2b4
Tolga Çakıroğlu <tolga.cakiroglu@diyalog.im>
2022-06-25 00:59:05 +0300

    new webserver address for zip files added

--- Package.swift
+++ Package.swift
@@ -26,22 +26,22 @@ let package = Package(
             dependencies: ["DiyalogSwiftPackages"]),
         .binaryTarget(
             name: "DiyalogEngine",
-            url: "https://diyalog.im/app/pkgs/ios_frameworks/DiyalogEngine/4.9.0/DiyalogEngine.zip",
+            url: "https://diyalog.im/releases/DiyalogEngine/4.9.0/DiyalogEngine.zip",
             checksum: "233b72202927cbc857e43b566d26ca0bf0e3b064781683e22980b42f8e5c3ef3"
         ),
         .binaryTarget(
             name: "j2objc",
-            url: "https://diyalog.im/app/pkgs/ios_frameworks/j2objc/1.0.3/j2objc.zip",
+            url: "https://diyalog.im/releases/j2objc/1.0.3/j2objc.zip",
             checksum: "7c6dc3a9050534bbe63905a91d0da57eeb078f4b41a3660324c709de9bbe9ba9"
         ),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://diyalog.im/app/pkgs/ios_frameworks/WebRTC/1.1.2941/WebRTC.zip",
+            url: "https://diyalog.im/releases/WebRTC/1.1.2941/WebRTC.zip",
             checksum: "9c5caa090782eefb518889d80afb6dad2311af132c8b0e89997777ff9d63ebbc"
         ),
         .binaryTarget(
             name: "opus",
-            url: "https://diyalog.im/app/pkgs/ios_frameworks/opus/1.3/opus.zip",
+            url: "https://diyalog.im/releases/opus/1.3/opus.zip",
             checksum: "adc7b8a7427ed9d81f1d67f861313574aaa913522495fb000f5f3be81c92fbeb"
         )
     ]
